d9de30f1223c8e7510478f8eb39b088b7b37f775
nnee <dvn281002@gmail.com>
2025-06-07 23:45:51 +0700

    Add member count to ClassroomEntity and update FilterClassrooms functionality

--- entity/classroom.go
+++ entity/classroom.go
@@ -9,7 +9,8 @@ type ClassroomEntity struct {
 	Name        string `json:"name"`
 	Description string `json:"description"`
 	AvatarUrl   string `json:"avatar_url"`
-	FullName    string `json:"full_name,omitempty"` // Full name of the user who created the classroom
+	FullName    string `json:"full_name,omitempty"`   // Full name of the user who created the classroom
+	MemberCount int64  `json:"member_count" gorm:"-"` // Number of members in the classroom
 }
 
 func (*ClassroomEntity) TableName() string {
--- repository/filter_classrooms.go
+++ repository/filter_classrooms.go
@@ -25,6 +25,7 @@ func (r *repositoryImpl) FilterClassrooms(ctx context.Context, params req.Filter
 		return nil, err
 	}
 
+	// Fill owner name
 	userIDs := make([]string, len(classrooms))
 	for i, classroom := range classrooms {
 		userIDs[i] = classroom.UserID
@@ -37,6 +38,39 @@ func (r *repositoryImpl) FilterClassrooms(ctx context.Context, params req.Filter
 
 	for i, member := range classrooms {
 		classrooms[i].FullName = userMap[member.UserID].FullName
+	}
+
+	// Fill member count
+	classroomIDs := make([]string, len(classrooms))
+	for i, classroom := range classrooms {
+		classroomIDs[i] = *classroom.ID
+	}
+
+	var memberCounts []struct {
+		ClassroomID string `json:"classroom_id"`
+		Count       int64  `json:"count"`
+	}
+
+	if err := r.Provider.Db.WithContext(ctx).
+		Model(&entity.ClassroomMemberEntity{}).
+		Select("classroom_id, COUNT(*) as count").
+		Where("classroom_id IN ?", classroomIDs).
+		Group("classroom_id").
+		Scan(&memberCounts).Error; err != nil {
+		return nil, err
+	}
+
+	memberCountMap := make(map[string]int64)
+	for _, v := range memberCounts {
+		memberCountMap[v.ClassroomID] = v.Count
+	}
+
+	for i, classroomID := range classroomIDs {
+		if count, ok := memberCountMap[classroomID]; ok {
+			classrooms[i].MemberCount = count
+		} else {
+			classrooms[i].MemberCount = 0
+		}
 	}
 
 	pagination.Rows = classrooms
